380c0c0b3b92f1b35328fc8f0fde5abf3785d4d7
Reid Beels <mail@reidbeels.com>
2015-05-18 01:13:49 -0700

    Simplify capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,44 +4,24 @@ lock '3.3.3'
 set :application, 'osbridge-volunteer'
 set :repo_url, 'git@github.com:reidab/osbridge-django-test.git'
 
-# Default branch is :master
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
-
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
 
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
 namespace :deploy do
 
-  after :restart, :clear_cache do
+  before :updated, :install_requirements do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+      within release_path do
+        execute "#{shared_path}/venv/bin/pip", "install", "-r requirements.txt"
+      end
+    end
+  end
+
+  after :published, :restart do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      within shared_path do
+        execute :touch, 'config/uwsgi_config.ini'
+      end
     end
   end
 
